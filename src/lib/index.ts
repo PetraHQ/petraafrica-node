f2f073e7f82e5963fc935041739c70a6b841550c
Emmanuel Adebayo <Emmanueltolu.adebayo@gmail.com>
2021-07-07 06:56:43 +0100

    chore: removed readme contents

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,17 @@
+import { Transaction } from "./modules/transaction";
 
 
 export class Petra{
+  //transaction: Transaction;
   constructor(secret_key:string) {
     if (!secret_key) {
       throw new Error('missing credentials, please pass in your credentials')
     }
   }
+
+
+
+ // this.transaction = new Transaction()
+
+
 }
